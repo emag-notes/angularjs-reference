220f90c431640f8f09a743f171c93a213f8db1af
emag <lanabe.lanabe@gmail.com>
2014-10-16 17:14:30 +0900

    Done 24/400

---
+++ chapter01/05_controller/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var MyController = function ($scope) {
+  $scope.message = 'Hello World!';
+  $scope.action = function() {
+    $scope.message = 'Goodbye, Everyone!'
+  };
+};
+
+var appModule = angular.module('app', []);
+appModule.controller('myController', MyController);
